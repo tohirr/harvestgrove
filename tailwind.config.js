24cc4f08075b7a7fbcc5ca919188220f2ed211c9
panda <tohirbabs@gmail.com>
2024-01-31 17:43:17 +0100

    tailwind and react-icons

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,14 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins"],
+        mono: ["Space Mono"],
+        dm: ["DM Sans"],
+        open: ["Open Sans"],
+      },
+    },
   },
   plugins: [],
 }
