07f0c195c5ef73771db29a3b07369e9c317fdabe
panda <tohirbabs@gmail.com>
2024-02-02 11:57:40 +0100

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,7 +33,7 @@ export const Header = () => {
         <div className="absolute lg:left-[25%] top-0">
         <h2 className="relative lg:leading-[5rem] text-3xl p-4 lg:text-6xl capitalize">nurturing nature, <br/> harvesting excelence</h2>
         <p className="relative text-xs lg:text-base lg:left-[22%] left-[5%] lg:max-w-[60%] max-w-[90%] p-2">At HarvestGrove, we are passionate about agriculture and committed to providing the highest quality produce and services to our customers. With a legacy of farming excellence spanning 10 years, </p>
-        <img src="/customers.jpg" alt="" className="relative lg:left-[45%] left-[35%] lg:pt-12 pt-10 w-[35%] lg:w-[25%]"/>
+        <img src="/customers.jpg" alt="" className="relative lg:left-[45%] left-[35%] lg:pt-10 pt-6 w-[35%] lg:w-[25%]"/>
         </div>
         <img src="/grovecard.jpg" alt="" className="absolute lg:rounded-2xl rounded-xl max-w-[50%] lg:top-[30%] top-[40%] left-[5%]" />
         <img src="/tag1.png" alt="" className="absolute lg:top-[65%] top-[75%] w-[6rem] right-[20vw]" />
