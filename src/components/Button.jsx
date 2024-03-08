6dd7020952eb55baca2938137e33a88cdbfeadc1
tohir babatunde <tohirbabs@gmail.com>
2024-03-08 14:27:28 +0100

    updates

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,13 +2,13 @@ const Button = ({icon, children, color, variant}) => {
     switch (variant) {
         case "underline":
             return (
-                <button className="group w-max flex flex-col gap-1"><span>{children}</span><div className="h-1 rounded-full w-0 transition-all duration-300  bg-[#0e0e0e]/80 group-hover:w-full"></div></button>
+                <button className="group text-xs md:text-base w-max flex flex-col gap-1"><span>{children}</span><div className="h-1 rounded-full w-0 transition-all duration-300  bg-[#0e0e0e]/80 group-hover:w-full"></div></button>
             )
             break;
     
         default:
             return (
-                <button className="group rounded-full w-max  border border-[#000] text-sm md:etxt-base text-black flex items-center gap-1 p-1 pl-4"><span>{children}</span><div className="md:p-3 p-2 rounded-full bg-[#0e0e0e]/30">{icon}</div></button>
+                <button className="group rounded-full w-max  border border-[#000] text-xs md:text-base text-black flex items-center gap-1 p-1 pl-4"><span>{children}</span><div className="md:p-3 p-2 rounded-full bg-[#0e0e0e]/30">{icon}</div></button>
                 )
             break;
     }
