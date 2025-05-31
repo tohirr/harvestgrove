2a114cd2af8565a8913d20be9402ea17579b6616
tohirr.xyz <tohirbabs@gmail.com>
2025-05-31 23:14:12 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
         <div className="flex gap-8 items-center">
           <div className="flex items-center gap-2 px-2">
             <img src="/leaflogo.svg" />
-            <h1 className="text-xl text-[#0EA91E] font-bold font-poppins  lg:block">ubuntufarms</h1>
+            <h1 className="text-xl text-[#0EA91E] font-bold font-poppins  lg:block">harvestgrove</h1>
           </div>
             <nav className="gap-5 hidden lg:flex">                     
           {navLinks.map((link, index)=> 
@@ -32,7 +32,7 @@ export const Header = () => {
         <img src="/mobilefarm.png" alt="" className="lg:hidden rounded-b-3xl"/>
         <div className="absolute lg:left-[25%] top-0">
         <h2 className="relative lg:leading-[5rem] text-2xl md:text-4xl md:p-4 p-3 lg:text-6xl capitalize">nurturing nature, <br/> harvesting excelence</h2>
-        <p className="relative text-sm lg:text-base lg:left-[22%] left-[5%] lg:max-w-[60%] max-w-[95%] p-2 leading-[1.2rem] lg:leading-[2rem]">At <span className="text-[#0EA91E] font-semibold ">ubuntufarms</span>, we are passionate about agriculture and committed to providing the highest quality produce and services to our customers. With a legacy of farming excellence spanning 10 years, </p>
+        <p className="relative text-sm lg:text-base lg:left-[22%] left-[5%] lg:max-w-[60%] max-w-[95%] p-2 leading-[1.2rem] lg:leading-[2rem]">At <span className="text-[#0EA91E] font-semibold ">harvestgrove</span>, we are passionate about agriculture and committed to providing the highest quality produce and services to our customers. With a legacy of farming excellence spanning 10 years, </p>
         <img src="/customers.jpg" alt="" className="relative lg:left-[45%] left-[35%] lg:pt-10 pt-5 w-[40%] lg:w-[25%]"/>
         </div>
         <img src="/grovecard.jpg" alt="" className="absolute lg:rounded-2xl rounded-xl max-w-[50%] lg:top-[30%] top-[40%] left-[5%]" />
